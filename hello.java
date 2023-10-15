6d326c22419777ab971e2e212ef458d94c58f2e2
kavya sree <71062304+kavya889@users.noreply.github.com>
2023-10-15 18:19:39 +0530

    adding the simple java file and class file

---
+++ hello.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class hello {
+    public static void main(String args[])
+    {
+        // Creating an empty Set
+        Set<String> s = new HashSet<String>();
+  
+        // Use add() method to add elements into the Set
+        s.add("Welcome");
+        s.add("To");
+        s.add("This");
+        s.add("Hello");
+        s.add("World");
+        s.add("Program");
+  
+        // Displaying the Set
+        System.out.println("Set: " + s);
+    }
+    
+}
